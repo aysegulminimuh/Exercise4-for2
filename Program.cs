fe36f1c71ec994f401642c447d01311b570f43d8
Ayşegül ÖZMEN <aysegulozmn@outlook.com>
2023-11-23 22:16:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿for (int i = 0; i < 1; i--)
+{
+    Console.Write("birinci sayıyı girin: ");
+    int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("ikinci sayıyı girin: ");
+    int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(sayi1 + sayi2);
+}
+
